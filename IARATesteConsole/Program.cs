a2960429de6f3790e6c91f11ad87058f8720fd19
Weberson Ribeiro <weberson.rs@outlook.com>
2021-05-17 09:59:45 -0300

    Alteraçao da classe ValidArrayWords

--- IARATesteConsole/Program.cs
+++ IARATesteConsole/Program.cs
@@ -8,21 +8,25 @@ namespace IARATesteConsole
     {
         static void Main(string[] args)
         {
-            //int[] values = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
-            int[] values = { 2, 8, 14 };
+            int[] values = { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+            //int[] values = { 2, 8, 14 };
 
             ValidArrayNumbers validArrayNumbers = new ValidArrayNumbers();
             var result = validArrayNumbers.ContainsValueImpar(values);
                     
             if (result.Count() > 0)
             {
-                Console.WriteLine("Foram encontrados os seguintes impares:");
+                Console.WriteLine("Foram encontrados os seguintes números impares:");
                 foreach (int i in result)
                     Console.WriteLine(i);
             }
             else
                 Console.WriteLine("Nenhum número impar encontrado");
-                        
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("=================================================");
+            Console.WriteLine("Palavras com letras duplicadas corrigidas");
             string[] words = { "abracadabra", "allottee", "assessee" };
             ValidArrayWords validArrayWords = new ValidArrayWords();
             var resultWords = validArrayWords.ContainsDuplicateValue(words);
--- IARATesteConsole/ValidArrayWords.cs
+++ IARATesteConsole/ValidArrayWords.cs
@@ -9,20 +9,25 @@ namespace IARATesteConsole
         public List<string> ContainsDuplicateValue(string[] values)
         {
             List<string> result = new List<string>(values);
+            List<string> words = new List<string>();
+            string word = string.Empty;
 
             foreach (string i in result)
             {
                 string valueWord = i;
+                word = string.Empty;
                 string previousLetter = string.Empty;
                 for (int x = 0; x < i.Length; x++)
                 {
-                    if (x > 0) 
+                    if (x > 0)
                         previousLetter = i.Substring(x - 1, 1).ToString();
-                    if (previousLetter.Equals(i.Substring(x, 1).ToString())) 
-                        valueWord.Remove(x);
+                    if (!previousLetter.Equals(i.Substring(x, 1).ToString()))
+                        word += i.Substring(x, 1).ToString();
                 }
+
+                words.Add(word);
             }
-            return result;
+            return words;
         }
     }
 }
